5584048f7e99bcfb6430c4685e974ba94536f251
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-01-12 18:52:23 -0800

    make submit button responsive and begin writing evaluation functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 // Run JS after DOM has loaded
 $(document).ready(function() {
+  var sideIDsArray = ["side-A", "side-B", "side-C"];
+  var sideData = [];
+
+  // function getSideData(sideIDsArray, sideData) {
+  //   getSideId.forEach(function(sideID, index) {
+  //     sideData[index] =
+  //   })
+  // }
+
+  $("form#triangle").submit(function(event) {
+    alert("#submit");
+  });
 
 }); // End JavaScript
